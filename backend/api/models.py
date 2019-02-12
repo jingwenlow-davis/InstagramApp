94bc2fd21992a1cd33e06644feeccbd6d0bff8fb
RishuvDutta <RishuvDutta@github.com>
2019-02-12 04:37:27 -0800

    added fields to user model

--- backend/api/models.py
+++ backend/api/models.py
@@ -9,11 +9,24 @@ from django.conf import settings
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=instance)
+	if created:
+		Token.objects.create(user=instance)
 
 # store users and passwords
 class User(AbstractUser):
+	location_radius = models.IntegerField(null=True, blank=True)
+	display_name = models.CharField(max_length=30, blank=True)
+	profile_picture = models.CharField(max_length=100, blank=True)
+	bio = models.TextField(max_length=500, blank=True)
+	age = models.IntegerField(null=True, blank=True)
+	age_range = models.IntegerField(null=True, blank=True)
+	
+	GENDER_CHOICES = (
+		('M', 'Male'),
+		('F', 'Female'),
+		('O', 'Other'),
+	)
+	gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
 
-    def __str__(self):
-        return self.username
+	def __str__(self):
+		return self.username
